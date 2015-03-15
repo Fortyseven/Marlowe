3b7268bc2021974658f689a028d8a94e18e54b8f
Fortyseven <fortyseven@gmail.com>
2015-03-15 19:50:52 -0400

    Imported list of hard-coded whois servers from Spade and verified their condition

--- Marlowe/Tools/Whois.cs
+++ Marlowe/Tools/Whois.cs
@@ -9,6 +9,42 @@ namespace Marlowe.Tools
 {
     class Whois
     {
+        // TO - timed out
+        // DNR - did not resolve
+        public string[] WhoisServers =
+        {
+            "whois.internic.net",
+            "whois.arin.net",
+            //"whois.ripe.net",       // TO
+            "whois.nic.am",
+            "whois.aunic.net",
+            //"whois.cdnnet.ca",      // DNR
+            "whois.nic.ch",
+            //"crsnic",               // DNR
+            //"whois.eunet.es",       // TO
+            "whois.nic.fr",
+            "whois.nic.gov",
+            "whois.apnic.net",
+            //"whois.nis.garr.it",    // DNR
+            //"whois.nic.ad.jp",      // TO
+            //"whois.nic.nm.kr",      // DNR
+            "whois.nic.li",
+            //"nic.ddn.mil",          // DNR
+            "whois.nic.mx",
+            "domain-registry.nl",
+            "whois.ripn.net",
+            //"whois.internic.se",    // DNR
+            "nanos.arnes.si",
+            "whois.thnic.net",
+            //"whois.nic.tj",         // DNR
+            //"whois.nic.uk",         // TO
+            "whois.ja.net",
+            "nii-server.isi.edu",
+            //"rwhois.exodus.net",    // TO
+            //"rwhois.digex.net",     // DNR
+            //"whois.geektools.com"   // TO
+        };
+
         public const string DEFAULT_SERVER = "whois.iana.org";
         public const int WHOIS_PORT = 43;
 
