4615c7cf810e0a7bc973f985ed8f7f8a6c945385
Fortyseven <fortyseven@gmail.com>
2015-03-15 22:54:06 -0400

    Added company name to assembly

--- Marlowe/Properties/AssemblyInfo.cs
+++ Marlowe/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle( "Marlowe" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "Bytes Templar" )]
 [assembly: AssemblyProduct( "Marlowe" )]
 [assembly: AssemblyCopyright( "Copyright ©  2015" )]
 [assembly: AssemblyTrademark( "" )]
